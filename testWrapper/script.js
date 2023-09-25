0f7af97be7d1b139b33b254d5aacc62c87a41f6e
Neal <tnharvey@gmail.com>
2023-09-24 21:47:38 -0500

    Add files via upload

---
+++ testWrapper/script.js
@@ -0,0 +1,32 @@
+let  checkSecure = false;
+let  checkPermissive = false;
+let  devMAC = "";
+
+// SecureBrowser.security.isEnvironmentSecure(function (state) {
+//         const secureState = JSON.parse(state);
+//         checkSecure = secureState.secure;
+//     });
+// SecureBrowser.security.getPermissiveMode(function (permissiveMode) {
+//         checkPermissive = permissiveMode;
+//     });
+// SecureBrowser.security.getMACAddress(function (addressArray) {
+//         devMAC = addressArray;
+//     });
+// Reference to the iframe
+
+// Function to launch the test
+function launchTest() {
+    // Set the source URL of the iframe to the test URL
+    testFrame.src = "https://forms.office.com/r/xrU9xr1a0B"; // Replace with your actual test URL
+}
+
+// Function to close the test
+function closeTest() {
+    // Clear the iframe source URL to close the test
+    SecureBrowser.security.close();
+}
+
+window.onload = function () {
+    let testFrame = document.getElementById("testFrame");
+    launchTest();
+}
