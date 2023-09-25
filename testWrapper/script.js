14acfa87182e769494605d1222c4c7b1db47fc22
Neal <tnharvey@gmail.com>
2023-09-24 21:57:00 -0500

    Update script.js

--- testWrapper/script.js
+++ testWrapper/script.js
@@ -15,13 +15,13 @@ let  devMAC = "";
 // Reference to the iframe
 
 // Function to launch the test
-function launchTest() {
+function launchTest () {
     // Set the source URL of the iframe to the test URL
     testFrame.src = "https://forms.office.com/r/0HhmYfaGRJ"; // Replace with your actual test URL
 }
 
 // Function to close the test
-function closeTest() {
+function closeTest () {
     // Clear the iframe source URL to close the test
     SecureBrowser.security.close();
 }
